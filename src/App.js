b68d89cef94bbd3b3bdbd2fee625f19509f990fa
Emilioj17 <ejjelves@gmail.com>
2021-07-27 07:09:59 -0400

    Inicio de Foundation con Citas

--- src/App.js
+++ src/App.js
@@ -2,22 +2,62 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Formulario = () => {
+    return (
+      <div className="translucent-form-overlay">
+      <form>
+        <h3>Crear Cita</h3>
+        <div className="">
+          <label>Nombre Mascota
+            <input type="text" name="keyword" placeholder="Nombre de tu Mascota" />
+          </label>
+        </div>
+        <div className="">
+          <label>Nombre Dueño
+            <input type="text" name="keyword" placeholder="Nombre del Dueño de la Mascota" />
+          </label>
+        </div>
+        <div className="">
+          <label>Fecha
+            <input type="date" name="keyword" />
+          </label>
+        </div>
+        <div className="">
+          <label>Hora
+            <input type="time" name="keyword" />
+          </label>
+        </div>
+        <div className="">
+          <label>Tipo de Mascota
+            <select name="status" type="text">
+              <option>Perro</option>
+              <option value="rent">Gato</option>
+              <option value="buy">Pajaro</option>
+            </select>
+          </label>
+        </div>
+        <div className="">
+          <label>Sintomas
+            <textarea placeholder="Escribe los sintomas de tu mascota aquí" rows="4"></textarea>
+          </label>
+        </div>
+        <button type="button" className="primary button expanded search-button">
+          Crear Cita
+        </button>
+      </form>
+    </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+        <div className="Header"><h1>Administrador de Pacientes</h1></div>
+        <div className="Izquierda">
+          <Formulario />
+        </div>
+        <div className="Derecha">
+          Hola
+        </div>
     </div>
   );
 }
